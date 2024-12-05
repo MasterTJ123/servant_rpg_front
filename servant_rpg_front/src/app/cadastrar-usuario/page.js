d34ab890194ebd824100220c01e8a38f180a5fed
Evaldo Souza <evaldo.souza2@gmail.com>
2024-12-04 23:14:12 -0300

    Acertando fonte do botao entrar

--- servant_rpg_front/src/app/cadastrar-usuario/page.js
+++ servant_rpg_front/src/app/cadastrar-usuario/page.js
@@ -1,103 +1,117 @@
-"use client"
+"use client";
 import "./cadastrar-usuario.css";
-import BotaoRedondo from "@/app/components/botaoRedondo/botaoRedondo";
-import {useState} from "react";
-import BotaoRedondoSubmit from "@/app/components/botaoRedondoSubmit/botaoRedondoSubmit";
+import BotaoRedondo from "../components/botaoRedondo/botaoRedondo";
+import { useState } from "react";
+import BotaoRedondoSubmit from "../components/botaoRedondoSubmit/botaoRedondoSubmit";
 
 export default function CadastroUsuario() {
-    const [formData, setFormData] = useState({
-        email: '',
-        nome: '',
-        senha: '',
-        confirmarSenha: ''
-    })
+  const [formData, setFormData] = useState({
+    email: "",
+    nome: "",
+    senha: "",
+    confirmarSenha: "",
+  });
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target
-        setFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const [erro, setErro] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Erro: As senhas devem coincidir
+    if (formData.senha !== formData.confirmarSenha) {
+      setErro("Erro! As senhas não coincidem!");
+      return;
     }
 
-    const [erro, setErro] = useState()
+    setErro("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        // Erro: As senhas devem coincidir
-        if (formData.senha !== formData.confirmarSenha) {
-            setErro('Erro! As senhas não coincidem!')
-            return
+    try {
+      const response = await fetch(
+        "http://localhost:8000/servant_rpg_back/cadastrarusuario",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formData.email,
+            nome: formData.nome,
+            senha: formData.senha,
+          }),
         }
+      );
 
-        setErro('')
+      if (!response.ok) {
+        const errorData = await response.json();
+        setErro(errorData["erro"]);
+        return;
+      }
 
-        try {
-            const response = await fetch('http://localhost:8000/servant_rpg_back/cadastrarusuario', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email: formData.email,
-                    nome: formData.nome,
-                    senha: formData.senha
-                })
-            })
+      const data = await response.json();
+      console.log(data);
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                setErro(errorData["erro"]);
-                return;
-            }
-
-            const data = await response.json()
-            console.log(data)
-
-            window.location.href = '/login'
-        } catch (error) {
-            console.error(error)
-            setErro('Erro inesperado! Tente novamente mais tarde!')
-        }
+      window.location.href = "/login";
+    } catch (error) {
+      console.error(error);
+      setErro("Erro inesperado! Tente novamente mais tarde!");
     }
-    return (
-        <div>
-            <div className="background-img"></div>
-            <div className="menu">
-                <BotaoRedondo url={"/login"} texto={"Voltar"}></BotaoRedondo>
-                <form>
-                    <div className="cadastro-div">
-                        <h3 className="cadastro">Cadastrar-se</h3>
-                        {erro && <div className="erro">{erro}</div>}
-                    </div>
-                    <input id="email"
-                           name="email"
-                           onChange={handleInputChange}
-                           value={formData.email}
-                           type="email"
-                           placeholder="E-mail"/>
-                    <input id="nome"
-                           name="nome"
-                           onChange={handleInputChange}
-                           value={formData.nome}
-                           type="text"
-                           placeholder="Nome"/>
-                    <input id="senha"
-                           name="senha"
-                           onChange={handleInputChange}
-                           value={formData.senha}
-                           type="password"
-                           placeholder="Senha"/>
-                    <input id="confirmarSenha"
-                           name="confirmarSenha"
-                           onChange={handleInputChange}
-                           value={formData.confirmarSenha}
-                           type="password"
-                           placeholder="Confirmar senha"/>
-                </form>
-                <BotaoRedondoSubmit handleSubmit={handleSubmit} texto={"Cadastrar"}></BotaoRedondoSubmit>
-            </div>
-        </div>
-    );
+  };
+  return (
+    <div>
+      <div className="background-img"></div>
+      <div className="menu">
+        <BotaoRedondo url={"/login"} texto={"Voltar"}></BotaoRedondo>
+        <form>
+          <div className="cadastro-div">
+            <h3 className="cadastro">Cadastrar-se</h3>
+            {erro && <div className="erro">{erro}</div>}
+          </div>
+          <input
+            id="email"
+            name="email"
+            onChange={handleInputChange}
+            value={formData.email}
+            type="email"
+            placeholder="E-mail"
+          />
+          <input
+            id="nome"
+            name="nome"
+            onChange={handleInputChange}
+            value={formData.nome}
+            type="text"
+            placeholder="Nome"
+          />
+          <input
+            id="senha"
+            name="senha"
+            onChange={handleInputChange}
+            value={formData.senha}
+            type="password"
+            placeholder="Senha"
+          />
+          <input
+            id="confirmarSenha"
+            name="confirmarSenha"
+            onChange={handleInputChange}
+            value={formData.confirmarSenha}
+            type="password"
+            placeholder="Confirmar senha"
+          />
+        </form>
+        <BotaoRedondoSubmit
+          handleSubmit={handleSubmit}
+          texto={"Cadastrar"}
+        ></BotaoRedondoSubmit>
+      </div>
+    </div>
+  );
 }
--- servant_rpg_front/src/app/utils/fetchPersonagens.tsx
+++ servant_rpg_front/src/app/utils/fetchPersonagens.tsx
@@ -1,6 +1,7 @@
 import personagens from "../../../public/personagens_model/personagens.json";
 
 export async function fetchPersonagens() {
+  //use essa função para buscar a lista de personagens cadastrados do banco
   //   const response = await fetch(
   //     "../../../public/personagens_model/personagens.json"
   //   ); // Replace with the actual path to your JSON or API
